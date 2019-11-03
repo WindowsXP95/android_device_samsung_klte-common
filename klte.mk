bed2e369a48dcfa7158e49e1a3a98a9d651bd323
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2019-11-03 12:20:24 +0200

    klte-common: Remove deprecated ipv6 tethering support * This is not a thing anymore since N.
    Change-Id: I8aa3cf828cb7f688f703544d9eda4170c99c7650

--- klte.mk
+++ klte.mk
@@ -52,11 +52,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FlipFlap
 
-# IPv6 tethering
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes
-
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
